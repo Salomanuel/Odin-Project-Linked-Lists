39961af9c157b7169f9f75a74374a5cf4d1439ce
Manuel <salomanuel.aux+code@gmail.com>
2016-10-12 19:45:07 +0200

    graphics revamp

--- exercises/hanoi.rb
+++ exercises/hanoi.rb
@@ -1,10 +1,23 @@
-def graphics
-	peg1 = [["x"],["xx"],["xxx"],["xxxx"],["xxxxx"]]
-	peg2 = [["o"],["o"],["o"],["o"],["o"]]
-	peg3 = [["o"],["o"],["o"],["o"],["o"]]
+def graphics(peg1=[1,2,3,4,5], peg2=[0,0,0,0,0], peg3=[0,0,0,0,0])
 	pegs = [peg1,peg2,peg3]
-	pegs.each { |j| puts j;puts "\n" }
 
+
+	pegs.each do |peg| 
+		peg.each_index do |i|
+			if peg[i] == 0
+				puts peg[i] = "O" 
+			else
+				puts peg[i] = "X" * peg[i]
+			end
+		end
+		puts "\n" 
+	end
+
+
+end
+
+def game
+	# if peg1.contains
 end
 
 graphics
