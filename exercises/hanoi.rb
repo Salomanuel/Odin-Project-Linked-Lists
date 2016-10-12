6e1d66717e709b5fb25031a314e3736869ca281e
Manuel <salomanuel.aux+code@gmail.com>
2016-10-12 20:00:01 +0200

    add basic game elements

--- exercises/hanoi.rb
+++ exercises/hanoi.rb
@@ -1,20 +1,25 @@
+def start
+	peg_gen(1)
+	game
+end
+
 def peg_gen(size)
-	peg1 = []
-	peg2 = []
-	peg3 = []
+	@peg1 = []
+	@peg2 = []
+	@peg3 = []
 	
 	size.times do |i|
-		peg1 << i+1
-		peg2 << 0
-		peg3 << 0
+		@peg1 << i+1
+		@peg2 << 0
+		@peg3 << 0
 	end
 
-	return graphics(peg1,peg2,peg3)
+	return graphics(@peg1,@peg2,@peg3)
 end
 
 def graphics(peg1, peg2, peg3)
 	pegs = [peg1,peg2,peg3]
-
+	puts peg1
 	pegs.each do |peg| 
 		peg.each_index do |i|
 			if peg[i] == 0
@@ -27,8 +32,20 @@ def graphics(peg1, peg2, peg3)
 	end
 end
 
-def game
-
+def turn
+	turn ||= 0
+	puts "turn \##{turn}"
+	turn +=1
 end
 
-peg_gen(2)
+def game
+	turn
+	if @peg1[0] == 1
+		@peg2[0] == 1
+		@peg1[0] == 0
+	end
+	graphics(@peg1,@peg2,@peg3)
+end
+
+start
+
