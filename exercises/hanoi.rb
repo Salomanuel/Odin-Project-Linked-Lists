d1e3035e75542606196d0df6a21a96e787a77f4d
Manuel <salomanuel.aux+code@gmail.com>
2016-10-12 19:11:55 +0200

    hanoi preliminary graphics

---
+++ exercises/hanoi.rb
@@ -0,0 +1,10 @@
+def graphics
+	peg1 = [["x"],["xx"],["xxx"],["xxxx"],["xxxxx"]]
+	peg2 = [["o"],["o"],["o"],["o"],["o"]]
+	peg3 = [["o"],["o"],["o"],["o"],["o"]]
+	pegs = [peg1,peg2,peg3]
+	pegs.each { |j| puts j;puts "\n" }
+
+end
+
+graphics
