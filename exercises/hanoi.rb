c8db6b7198208e683d707cc403c0dde19dd671a4
Manuel <salomanuel.aux+code@gmail.com>
2016-10-12 19:50:30 +0200

    add peg generator using dynamic peg size

--- exercises/hanoi.rb
+++ exercises/hanoi.rb
@@ -1,6 +1,19 @@
-def graphics(peg1=[1,2,3,4,5], peg2=[0,0,0,0,0], peg3=[0,0,0,0,0])
-	pegs = [peg1,peg2,peg3]
+def peg_gen(size)
+	peg1 = []
+	peg2 = []
+	peg3 = []
+	
+	size.times do |i|
+		peg1 << i+1
+		peg2 << 0
+		peg3 << 0
+	end
 
+	return graphics(peg1,peg2,peg3)
+end
+
+def graphics(peg1, peg2, peg3)
+	pegs = [peg1,peg2,peg3]
 
 	pegs.each do |peg| 
 		peg.each_index do |i|
@@ -12,12 +25,11 @@ def graphics(peg1=[1,2,3,4,5], peg2=[0,0,0,0,0], peg3=[0,0,0,0,0])
 		end
 		puts "\n" 
 	end
-
-
 end
 
 def game
-	# if peg1.contains
+
+	
 end
 
-graphics
+peg_gen(5)
