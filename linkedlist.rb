db977fff5603cef39375ec508e8e550cf0a3ce87
Manuel <salomanuel.aux+code@gmail.com>
2016-10-19 18:16:02 +0200

    move files to recursion and begin linkedlist.rb

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,15 +1,28 @@
 class LinkedList
-	def append
+	def initialize
+		@head = nil
+		@tail = nil
 	end
+
+	class Node
+		def initialize(data)
+			@next  = nil
+			@value = nil
+		end
+
+		def append
+
+		end
+
+
+		def value(data)
+			@value = data
+		end
+
+		def next_node
+			return nil
+		end
+	end
+	
 end
 
-class Node
-	def value
-		return nil
-	end
-
-	def next_node
-		return nil
-	end
-
-end
