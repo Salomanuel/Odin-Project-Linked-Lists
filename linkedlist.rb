9ba62883ef84aaab6e4b7bd0899fd40ad5dc45d7
Manuel <salomanuel.aux+code@gmail.com>
2016-10-21 13:46:52 +0200

    official linkedlist now has append, prepend and to_s

--- linkedlist.rb
+++ linkedlist.rb
@@ -7,8 +7,8 @@ You will need two classes:
 *  default.
 
 Build the following methods in your linked list class:
-  #append adds a new node to the end of the list
-  #prepend adds a new node to the start of the list
+* #append adds a new node to the end of the list
+*  #prepend adds a new node to the start of the list
   #size returns the total number of nodes in the list
   #head returns the first node in the list
   #tail returns the last node in the list
@@ -18,7 +18,7 @@ Build the following methods in your linked list class:
   	is in the list and otherwise returns false.
   #find(data) returns the index of the node 
   	containing data, or nil if not found.
-  #to_s represent your LinkedList objects as 
+* #to_s represent your LinkedList objects as 
   	strings, so you can print them out and 
   	preview them in the console. The format 
   	should be: 
@@ -26,31 +26,49 @@ Build the following methods in your linked list class:
 =end
 
 class LinkedList
-	def initialize
-		@head = Node.new("head")
-		@tail = Node.new("tail")
+	def initialize(node)
+		raise "list should be initialized with a NODE" if node.class != Node
+		@head = node
+		@tail = node
 	end
 
-	class Node
-		def initialize(data)
-			@next  = nil
-			@value = nil
-		end
-
-		def append
-
-
-
-		end
-
-
-		def value(data)
-			@value = data
-		end
-
-		def next_node
-		end
+	def append(node)
+		@tail.next = node
+		@tail = @tail.next
 	end
-	
+
+	def prepend(node)
+		temp = @head
+		@head = node
+		@head.next = temp
+	end
+
+	def to_s
+		print @head.value
+		while @head.next != nil
+			print " => #{@head.next.value}"
+			@head = @head.next
+		end
+		puts
+	end
 end
+
+class Node
+	attr_accessor :next, :value
+	def initialize(value)
+		@value = value
+	end
+
+	# def next_node
+	# end
+end
+
+list = LinkedList.new(node1 = Node.new("nodeFirst"))
+1.upto(4) { |i| list.append(Node.new("node#{i}")) }
+
+list.prepend(Node.new("pre_node1"))
+
+# puts node1.value
+
+list.to_s
 
