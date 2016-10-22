f1e66d401cf0e2b09632128eb110a0eb9a0e7329
Manuel <salomanuel.aux+code@gmail.com>
2016-10-22 20:09:47 +0200

    add at(index) method

--- exercises/22-sat-linked_list.rb
+++ exercises/22-sat-linked_list.rb
@@ -42,10 +42,13 @@ class LinkedList
 		return ary
 	end
 
-	def size
-		return self.iter.length
+	def size	
+		return self.iter.length 
 	end
 
+	def at(index)	
+		return self.iter[index].value
+	end
 
 	def to_s
 		first = @head
@@ -70,3 +73,5 @@ list.to_s
 puts "head is #{list.head.value}"
 puts "tail is #{list.tail.value}"
 puts "list size is #{list.size}"
+puts "at position 2 is #{list.at(2)}"
+puts "at position 0 is #{list.at(0)}"
